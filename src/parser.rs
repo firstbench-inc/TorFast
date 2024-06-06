92e40fda1a5af55d208dcfb075ed128f7ac48e87
Username-08 <yashranjan08@proton.me>
2024-06-06 15:55:10 +0530

    change parser params

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,6 @@
 use rcdom::{Handle, NodeData};
 
-pub fn extract_tags<S: Into<&'static str>>(
+pub fn extract_a_tags<S: Into<&'static str>>(
     handle: Handle,
     tag: S,
     tags: &mut Vec<String>,
@@ -21,6 +21,32 @@ pub fn extract_tags<S: Into<&'static str>>(
                         tags.push(attr.value.to_string());
                         println!("{}", attr.value);
                     });
+            }
+        }
+        _ => {}
+    }
+    for child in node.children.borrow().iter() {
+        extract_tags(child.clone(), "a", tags);
+    }
+}
+
+pub fn extract_tags<S: Into<&'static str>>(
+    handle: Handle,
+    tag: S,
+    tags: &mut Vec<String>,
+) {
+    let node = handle;
+    match node.data {
+        NodeData::Element {
+            ref name,
+            ref attrs,
+            ..
+        } => {
+            if &name.local == tag.into() {
+                attrs.borrow().iter().for_each(|attr| {
+                    tags.push(attr.value.to_string());
+                    println!("{}", attr.value);
+                });
             }
         }
         _ => {}
