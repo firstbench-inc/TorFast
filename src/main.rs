eab353921d4f2c8b94e3e35863fca8baf20c047c
Username-08 <yashranjan08@proton.me>
2024-06-07 13:32:05 +0530

    add event loop

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ mod tests;
 mod parser;
 mod fetcher;
 mod parser2;
-use std::collections::HashMap;
+use core::panic;
+use std::collections::{HashMap, VecDeque};
 
-use parser::extract_tags;
+use parser::{extract_a_tags, extract_tags};
 use tokio;
 extern crate markup5ever_rcdom as rcdom;
 
@@ -12,22 +13,21 @@ use html5ever::tendril::TendrilSink;
 use rcdom::RcDom;
 use std::default::Default;
 
-const SEEDLIST: [&str;11] = [
-    "http://torlinkv7cft5zhegrokjrxj2st4hcimgidaxdmcmdpcrnwfxrr2zxqd.onion/",
-    "http://fvrifdnu75abxcoegldwea6ke7tnb3fxwupedavf5m3yg3y2xqyvi5qd.onion/",
-    "http://zqktlwiuavvvqqt4ybvgvi7tyo4hjl5xgfuvpdf6otjiycgwqbym2qad.onion/wiki/index.php/Main_Page",
-    "http://3bbad7fauom4d6sgppalyqddsqbf5u5p56b5k5uk2zxsy3d6ey2jobad.onion/discover",
-    "http://tt3j2x4k5ycaa5zt.onion/",
-    "http://juhanurmihxlp77nkq76byazcldy2hlmovfu2epvl5ankdibsot4csyd.onion/address/",
-    "http://juhanurmihxlp77nkq276byazcldy2hlmovfu2epvl5ankdibsot4csyd.onion/add/onionsadded/",
-    "http://donionsixbjtiohce24abfgsffo2l4tk26qx464zylumgejukfq2vead.onion/?cat=19&pg=1",
-    "http://donionsixbjtiohce24abfgsffo2l4tk26qx464zylumgejukfq2vead.onion/?cat=20&pg=1&lang=en",
-    "http://donionsixbjtiohce24abfgsffo2l4tk26qx464zylumgejukfq2vead.onion/?cat=7&pg=1&lang=en",
-    "https://github.com/alecmuffett/real-world-onion-sites",
-];
-
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
+    let SEEDLIST: [String;11] = [
+        String::from("http://torlinkv7cft5zhegrokjrxj2st4hcimgidaxdmcmdpcrnwfxrr2zxqd.onion/"),
+        String::from("http://fvrifdnu75abxcoegldwea6ke7tnb3fxwupedavf5m3yg3y2xqyvi5qd.onion/"),
+        String::from("http://zqktlwiuavvvqqt4ybvgvi7tyo4hjl5xgfuvpdf6otjiycgwqbym2qad.onion/wiki/index.php/Main_Page"),
+        String::from("http://3bbad7fauom4d6sgppalyqddsqbf5u5p56b5k5uk2zxsy3d6ey2jobad.onion/discover"),
+        String::from("http://tt3j2x4k5ycaa5zt.onion/"),
+        String::from("http://juhanurmihxlp77nkq76byazcldy2hlmovfu2epvl5ankdibsot4csyd.onion/address/"),
+        String::from("http://juhanurmihxlp77nkq276byazcldy2hlmovfu2epvl5ankdibsot4csyd.onion/add/onionsadded/"),
+        String::from("http://donionsixbjtiohce24abfgsffo2l4tk26qx464zylumgejukfq2vead.onion/?cat=19&pg=1"),
+        String::from("http://donionsixbjtiohce24abfgsffo2l4tk26qx464zylumgejukfq2vead.onion/?cat=20&pg=1&lang=en"),
+        String::from("http://donionsixbjtiohce24abfgsffo2l4tk26qx464zylumgejukfq2vead.onion/?cat=7&pg=1&lang=en"),
+        String::from("https://github.com/alecmuffett/real-world-onion-sites"),
+    ];
     // Make sure you are running tor and this is your socks port
     // let proxy = reqwest::Proxy::all("socks5h://127.0.0.1:9050").expect("tor proxy should be there");
     // let client = reqwest::Client::builder()
@@ -61,14 +61,14 @@ async fn main() -> Result<(), reqwest::Error> {
     //     .unwrap();
     //
     // parser::extract_tags(dom.document, "a",  &mut tags);
-    let mut to_visit = vec![];
+    let mut to_visit = VecDeque::from(SEEDLIST);
     start_crawler(to_visit).await?;
     Ok(())
 }
 
 
 
-pub async fn start_crawler(to_visit: Vec<&str>) -> Result<(), reqwest::Error> {
+pub async fn start_crawler(mut to_visit: VecDeque<String>) -> Result<(), reqwest::Error> {
     let proxy = reqwest::Proxy::all("socks5h://127.0.0.1:9050").expect("tor proxy should be there");
     let client = reqwest::Client::builder()
         .proxy(proxy)
@@ -80,12 +80,38 @@ pub async fn start_crawler(to_visit: Vec<&str>) -> Result<(), reqwest::Error> {
     let is_tor = text.contains("Congratulations. This browser is configured to use Tor.");
     println!("Is Tor: {is_tor}");
 
-    for seed in SEEDLIST {
-        println!("{}", seed);
-        let res = client.get(seed).send().await?;
-        println!("sent resp to {}", seed);
+    let mut url = String::new();
+    loop {
+        url = match to_visit.pop_front() {
+            Some(url) => url,
+            None => panic!("seedlist exhausted")
+        };
+        let res = client.get(url).send().await?;
         let t = res.text().await?;
-        println!("{:?}", t);
+
+        let title = extract_title(&t);
+
+        let mut page_data = HashMap::new();
+        page_data.insert("link".to_string(), url.to_string());
+        page_data.insert("content".to_string(), t.clone());
+        if let Some(title_str) = title {
+            page_data.insert("title".to_string(), title_str);
+        }
+
+        post_url_data(&client, &page_data).await?;
+        let mut links = vec![];
+
+        let dom = html5ever::parse_document(RcDom::default(), Default::default())
+            .from_utf8()
+            .read_from(&mut t.as_bytes())
+            .unwrap();
+
+        extract_a_tags(dom.document, &mut links);
+        to_visit.append(&mut VecDeque::from(links));
+    }
+    for seed in SEEDLIST {
+        let res = client.get(seed).send().await?;
+        let t = res.text().await?;
 
         let title = extract_title(&t);
 
@@ -101,6 +127,7 @@ pub async fn start_crawler(to_visit: Vec<&str>) -> Result<(), reqwest::Error> {
     Ok(())
 
 }
+
 async fn post_url_data(client: &reqwest::Client, data: &HashMap<String, String>) -> Result<(), reqwest::Error> {
     let res = client
         .post("http://127.0.0.1:9200/logs/_doc")
@@ -110,6 +137,7 @@ async fn post_url_data(client: &reqwest::Client, data: &HashMap<String, String>)
     println!("Posted data: {:?}", res);
     Ok(())
 }
+
 fn extract_title(html: &str) -> Option<String> {
     let dom = html5ever::parse_document(RcDom::default(), Default::default())
         .from_utf8()
@@ -117,9 +145,9 @@ fn extract_title(html: &str) -> Option<String> {
         .unwrap();
 
     let document = dom.document;
-    let mut title = None;
+    let mut title = vec![];
 
     parser::extract_tags(document, "title", &mut title);
 
-    title
+    title.pop()
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,10 +1,6 @@
 use rcdom::{Handle, NodeData};
 
-pub fn extract_a_tags<S: Into<&'static str>>(
-    handle: Handle,
-    tag: S,
-    tags: &mut Vec<String>,
-) {
+pub fn extract_a_tags(handle: Handle, tags: &mut Vec<String>) {
     let node = handle;
     match node.data {
         NodeData::Element {
@@ -12,21 +8,21 @@ pub fn extract_a_tags<S: Into<&'static str>>(
             ref attrs,
             ..
         } => {
-            if &name.local == tag.into() {
+            if &name.local == "a" {
                 attrs
                     .borrow()
                     .iter()
                     .filter(|x| x.name.local.to_string() == "href")
                     .for_each(|attr| {
                         tags.push(attr.value.to_string());
-                        println!("{}", attr.value);
+                        // println!("{}", attr.value);
                     });
             }
         }
         _ => {}
     }
     for child in node.children.borrow().iter() {
-        extract_tags(child.clone(), "a", tags);
+        extract_a_tags(child.clone(), tags);
     }
 }
 
@@ -36,13 +32,14 @@ pub fn extract_tags<S: Into<&'static str>>(
     tags: &mut Vec<String>,
 ) {
     let node = handle;
+    let tag: &str = tag.into();
     match node.data {
         NodeData::Element {
             ref name,
             ref attrs,
             ..
         } => {
-            if &name.local == tag.into() {
+            if &name.local == tag {
                 attrs.borrow().iter().for_each(|attr| {
                     tags.push(attr.value.to_string());
                     println!("{}", attr.value);
@@ -52,6 +49,6 @@ pub fn extract_tags<S: Into<&'static str>>(
         _ => {}
     }
     for child in node.children.borrow().iter() {
-        extract_tags(child.clone(), "a", tags);
+        extract_tags(child.clone(), tag, tags);
     }
 }
